9e94948cd7c37b009b1c0c86387e10d9c9a2c952
Siar Sohrab <siar.sohrab@gmail.com>
2021-02-26 15:43:57 +0100

    done ex-2

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,20 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+function tellfortune(work, location, partner, children){
+    this.work = work
+    this.location = location
+    this.partner = partner
+    this.children = children
+    this.fortune = function(){
+    window.alert("you will be a " + this.work +" in " + this.location +", and married to "+this.partner + "with " +this.children +" kids. ");
+
+    }
+}
+var work = prompt("your work");
+var loc = prompt("where do you want to work");
+var partner = prompt("your partner name ");
+var children = prompt("how many children you have");
+
+var sohrab = new tellfortune(work, loc, partner, children);
+sohrab.fortune()
