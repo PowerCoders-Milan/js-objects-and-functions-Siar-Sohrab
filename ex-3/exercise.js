ad2c2df36968ec6e2ed168d92fafff67525d6d50
Siar Sohrab <siar.sohrab@gmail.com>
2021-02-26 17:18:57 +0100

    ex-3 done

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,22 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+var color = ['blu', 'green', 'yellow']
+console.log(color[0] + ' is my first choice color!')
+console.log(color[1] + ' is my secod choice color!')
+console.log(color[2] + ' is my third choice color!')
+
+//next steps
+var favorite ={
+    color: ' blue ',
+    pet: 'dog ',
+    season: ' winter ',
+    president: ' trump ',
+
+}
+
+document.write(favorite.color)
+document.write(favorite.pet)
+document.write(favorite.season)
+document.write(favorite.president)
