e10cc1d021bbe4abf6bb8f3f031e9661018e8992
Siar Sohrab <siar.sohrab@gmail.com>
2021-02-26 14:53:19 +0100

    done ex-1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,25 @@
-// write your JS here
-// remember to always test in the browser
-// to see if everything works correctly
+
+
+function grammatical(noun, verb, adverb, adjective,) {
+    return (noun+' '+ verb+ ' '+ adverb+' '+ adjective); 
+}
+var noun = prompt("please write a noun");
+var verb = prompt("please write a verb");
+var adverb = prompt("please write an adverb");
+var adjective = prompt("please write an ajective");
+window.alert(grammatical (noun, verb, adverb, adjective));
+console.log(grammatical (noun, verb, adverb, adjective));
+
+
+
+
+
+
+
+
+
+
+
+  //Create a simple function that prompts the user for a noun, a verb, an adverb, and an adjective and injects those into a story that you create.
+
+//Ask the user to give you the elements and use a function to run the code and display the result on the browser.
